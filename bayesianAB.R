fbd224791fb62e2aaafd4d5798345d42f79aadf7
Karl Kastan <karlkastan@gmail.com>
2015-10-16 16:36:06 -0700

    initial commit

---
+++ bayesianAB.R
@@ -0,0 +1,18 @@
+# See http://www.evanmiller.org/bayesian-ab-testing.html
+#
+# Formula for computing the Bayesian probability that A is better than B
+#
+# α_A - one plus the number of successes for A
+# β_A - one plus the number of failures for A
+# α_B - one plus the number of successes for B
+# β_B - one plus the number of failures of B
+
+a_better_than_b <- function(α_A, β_A, α_B, β_B) {
+  val <- 0
+
+  for (i in 0:α_B-1) {
+    val <- val + exp(lbeta(α_A + i, β_B + β_A) - log(β_B + i) - lbeta(1+i, β_B) - lbeta(α_A, β_A))
+  }
+
+  return (val)
+}
